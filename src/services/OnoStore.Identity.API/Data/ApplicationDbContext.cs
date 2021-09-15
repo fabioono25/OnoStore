52d0bc28263b91b018cdbb2be72aacfcf7157b4f
fabioono25 <fabioono25@gmail.com>
2021-09-15 22:15:19 +0100

    adding context, support to identity and migrations

---
+++ src/services/OnoStore.Identity.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnoStore.Identity.API.Data
+{
+    // EF Context
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) {}
+    }
+}
