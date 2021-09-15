44fbdb74179ddbe741a95c94b56eb12e392b156f
fabioono25 <fabioono25@gmail.com>
2021-09-15 22:35:41 +0100

    adding view models, support to registry and authentication

---
+++ src/services/OnoStore.Identity.API/Controllers/AutoController.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnoStore.Identity.API.Models.UserViewModels;
+
+namespace OnoStore.Identity.API.Controllers
+{
+    [Route("api/identity")]
+    public class AutoController: Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AutoController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("new-account")]
+        public async Task<ActionResult> Register(UserRegistry userRegistry)
+        {
+            if (!ModelState.IsValid) return BadRequest();//CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = userRegistry.Mail,
+                Email = userRegistry.Mail,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, userRegistry.Password);
+
+            if (result.Succeeded)
+            {
+                // await _signInManager.SignInAsync(user, false); - it's possible to login now
+                return Ok();
+                //return CustomResponse(await GerarJwt(usuarioRegistro.Email));
+            }
+
+            //foreach (var error in result.Errors)
+            //{
+            //    AdicionarErroProcessamento(error.Description);
+            //}
+
+            //return CustomResponse();
+            return BadRequest();
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<ActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();//CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(userLogin.Mail, userLogin.Password,
+                false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+                //return CustomResponse(await GerarJwt(usuarioLogin.Email));
+            }
+
+            //if (result.IsLockedOut)
+            //{
+            //    AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas");
+            //    return CustomResponse();
+            //}
+
+            //AdicionarErroProcessamento("Usuário ou Senha incorretos");
+            //return CustomResponse();
+
+            return BadRequest();
+        }
+    }
+}
---
+++ src/services/OnoStore.Identity.API/Models/UserViewModels/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnoStore.Identity.API.Models.UserViewModels
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "Field {0} with wrong format")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Field {0} is mandatory")]
+        [StringLength(100, ErrorMessage = "Field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
