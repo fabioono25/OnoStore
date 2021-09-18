53e73e35449eafe8c0ca369540d9f59b001e20a3
fabioono25 <fabioono25@gmail.com>
2021-09-18 09:34:39 +0100

    Adding cookie support to the application

---
+++ src/web/OnoStore.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnoStore.WebApp.MVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        //public static void RegisterServices(this IServiceCollection services)
+        //{
+        //    services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+        //    services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        //    services.AddScoped<IUser, AspNetUser>();
+        //}
+    }
+}
