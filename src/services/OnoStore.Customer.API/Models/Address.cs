e5e32f24f55da55bb6e74a6b5cced7dab84fd77c
fabioono25 <fabioono25@gmail.com>
2021-09-24 14:22:06 +0100

    adding entities, aggregate root, domain objects, VO

---
+++ src/services/OnoStore.Customer.API/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OnoStore.Core.DomainObjects;
+
+namespace OnoStore.Customer.API.Models
+{
+    public class Address : Entity
+    {
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string Neighbor { get; private set; }
+        public string ZipCode { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public Guid ClientId { get; private set; }
+
+        // EF Relation
+        public Client Client { get; protected set; }
+
+        public Address(string street, string number, string complement, string neighbor, string cep, string city, string state)
+        {
+            Street = street;
+            Number = number;
+            Complement = complement;
+            Neighbor = neighbor;
+            ZipCode = cep;
+            City = city;
+            State = state;
+        }
+    }
+}
---
+++ src/services/OnoStore.Customer.API/Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OnoStore.Core.DomainObjects;
+
+namespace OnoStore.Customer.API.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
+        public bool Deleted { get; private set; }
+        public Address Address { get; private set; }
+
+        // EF Relation
+        protected Client() { }
+
+        public Client(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
+            Deleted = false;
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AttributeAddress(Address address)
+        {
+            Address = address;
+        }
+    }
+}
